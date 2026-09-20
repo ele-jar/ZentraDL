e9e82d4744f9e2ff5fc828fd336427bd273b26f3
ZentraDL Builder <builder@zentradl.local>
2026-09-20 09:52:34 +0000

    fix(hilt): import android.app.Service in DownloadService

--- android/app/src/main/kotlin/com/elejar/ZentraDL/service/DownloadService.kt
+++ android/app/src/main/kotlin/com/elejar/ZentraDL/service/DownloadService.kt
@@ -2,6 +2,7 @@ package com.elejar.ZentraDL.service
 
 import android.app.Notification
 import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
