ce1279614135cc123712cdac41cf5bc0b1a3ed4f
ZentraDL Builder <builder@zentradl.local>
2026-09-20 23:01:14 +0000

    fix(di): missing BtOptions import

--- android/app/src/main/kotlin/com/elejar/ZentraDL/di/AppModule.kt
+++ android/app/src/main/kotlin/com/elejar/ZentraDL/di/AppModule.kt
@@ -14,6 +14,7 @@ import com.elejar.ZentraDL.engine.http.HttpDownloader
 import com.elejar.ZentraDL.engine.http.SpeedLimiter
 import com.elejar.ZentraDL.engine.model.Downloader
 import com.elejar.ZentraDL.engine.torrent.BtEngine
+import com.elejar.ZentraDL.engine.torrent.BtOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
