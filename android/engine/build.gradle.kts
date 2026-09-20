0a7e641e5fee60b4af449dfc81866d97cecfde58
ZentraDL Builder <builder@zentradl.local>
2026-09-20 09:09:57 +0000

    ci(debug): FULL test logging via tasks.withType<Test>

--- android/engine/build.gradle.kts
+++ android/engine/build.gradle.kts
@@ -9,13 +9,9 @@ android {
     defaultConfig { minSdk = libs.versions.minSdk.get().toInt() }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17) } }
-    testOptions {
-        unitTests.all {
-            testLogging {
-                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-                showCauses = true
-                showStackTraces = true
-            }
+    tasks.withType<org.gradle.api.tasks.testing.Test> {
+        testLogging {
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
         }
     }
 }
